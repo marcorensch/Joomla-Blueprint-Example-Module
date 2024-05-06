bb9ef2a26e4f73601a2494f817e805aac9237444
Marco Rensch <marco.rensch@nx-designs.ch>
2024-05-06 07:47:51 +0200

    added grid grid and table layouts

---
+++ tmpl/grid.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package                                     NXD BluePrint Module
+ *
+ * @author                                      [CREATOR-COMPANY] | [CREATOR-FULLNAME] <[CREATOR-EMAIL]>
+ * @copyright                                   Copyright(R) [CREATED-YEAR] by [CREATOR-COMPANY] | [CREATOR-FULLNAME]
+ * @license                                     GNU General Public License version 2 or later; see LICENSE.txt
+ * @link                                        [CREATOR-URL]
+ * @since                                       [EXTENSION-VERSION]
+ *
+ */
+
+defined('_JEXEC') or die;
