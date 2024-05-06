be5869f4f3106d9809295bfeb2f589f4ba08df29
Marco Rensch <marco.rensch@nx-designs.ch>
2024-05-06 07:36:38 +0200

    added console logs

---
+++ media/mod_blueprint/js/es6_module.js
@@ -0,0 +1,9 @@
+// Example of ES6 module
+
+class ES6Module {
+    constructor() {
+        console.log('ES6 module loaded');
+    }
+}
+
+export { ES6Module }
---
+++ media/mod_blueprint/js/main.js
@@ -0,0 +1,6 @@
+// Example JS file
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM loaded');
+    console.log('Example JS file loaded');
+});
