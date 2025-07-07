2fae13f440385028a623b01d48614ddc086083a4
Proximate <marco.rensch@area59.ch>
2025-07-07 10:22:41 +0200

    Update install.mysql.utf8.sql
    text has no default - error on install

--- sql/install.mysql.utf8.sql
+++ sql/install.mysql.utf8.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `#__mod_blueprint_data`
     `id`           int(11)                                                NOT NULL AUTO_INCREMENT,
     `title`        varchar(255)                                           NOT NULL DEFAULT '',
     `alias`        varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
-    `params`       text                                                   NOT NULL DEFAULT '',
+    `params`       text,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
